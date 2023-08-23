0ce7348884a41ffd332baa035589184f6a0eae99
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-08-24 01:40:57 +0200

    User class added

---
+++ LibraryManagement/src/User.java
@@ -0,0 +1,47 @@
+public class User {
+    
+    private String name;
+    private int age;
+    private String email;
+    private String phoneNumber;
+
+    public User(String name, int age, String email, String phoneNumber){
+        this.name = name; 
+        this.age = age;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+}
