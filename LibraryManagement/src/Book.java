39d7827dc25f5858f1e09adc965fa264b090c2ce
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-08-23 17:02:40 +0200

    Book and BookManagement: Implemented

---
+++ LibraryManagement/src/Book.java
@@ -0,0 +1,39 @@
+public class Book {
+
+    // Attributes
+    private String title;
+    private String author;
+    private int pages;
+
+    // Constructor
+    public Book (String title, String author, int pages){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+    
+    public int getPages(){
+        return pages;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public void setPages(int pages){
+        this.pages = pages;
+    }
+
+}
---
+++ LibraryManagement/src/BookManagement.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+import java.util.Map;
+
+interface BookManage {
+    
+    public void addBook(String title, String author, int pages);
+    public void removeBook(String title);
+    public void updateBook(String title, String author, int pages);
+
+}
+
+public class BookManagement implements BookManage {
+
+    private Map<String, Book> store = new HashMap<>();
+
+    @Override
+    public void addBook(String title, String author, int pages){
+
+        Book book = new Book(title, author, pages);
+        store.put(title, book);
+
+    }
+
+    @Override
+    public void removeBook(String title){
+
+        // Check if the book is in the store
+        if (store.containsKey(title)){
+            store.remove(title);   
+        } else {
+            System.out.println("Book not found " + title);
+        }
+
+    }
+
+    @Override 
+    public void updateBook(String title, String author, int pages){
+
+        // Check if the book is in the store
+        if (store.containsKey(title)){
+            Book book = getBook(title);
+            book.setAuthor(author);
+            book.setPages(pages);
+        } else {
+            System.out.println("Book not found: " + title);
+        }
+
+    }
+
+    // Additional methods for encapsulation
+    public Book getBook(String title) {
+        return store.get(title);
+    }
+
+    public int getNumberOfBooks() {
+        return store.size();
+    }
+    
+
+}
