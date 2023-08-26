ef3db978bb6bea65acf13bc64b89bbc71bcc19fd
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-08-26 17:01:21 +0200

    Don't allow to registering more than one user with the same user ID

--- LibraryManagement/src/UserManagement.java
+++ LibraryManagement/src/UserManagement.java
@@ -12,14 +12,25 @@ public class UserManagement implements UserManage {
 
     @Override
     public void registerUser(LibraryData libraryData, String userId, String name, int age, String email, String phoneNumber){
-        User user = new User(name, age, email, phoneNumber);
-        libraryData.getUsers().put(userId, user);
-        System.out.println("User registered correctly.");
+
+        // Check that the user is not already registered
+        if (!libraryData.getUsers().containsKey(userId)){
+
+            User user = new User(name, age, email, phoneNumber);
+            libraryData.getUsers().put(userId, user);
+            System.out.println("User registered correctly.");
+
+        } else{
+            System.out.println("The user already exists: " + userId);
+        }
+
+
     }
 
     @Override
     public void updateUser(LibraryData libraryData, String userId, String name, int age, String email, String phoneNumber){
 
+        // Check if the user exists
         if (libraryData.getUsers().containsKey(userId)){
             
             User user = libraryData.getUsers().get(userId);
@@ -38,6 +49,7 @@ public class UserManagement implements UserManage {
     @Override
     public void deactivateUser(LibraryData libraryData, String userId){
 
+        // Check if the user exists
         if (libraryData.getUsers().containsKey(userId)){
             libraryData.getUsers().remove(userId);
             System.out.println("User deactivated correctly.");
